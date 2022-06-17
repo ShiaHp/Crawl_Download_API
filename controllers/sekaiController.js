5977436bcc062724a772256821a85b9ab7039cc7
Huy Phúc <thienxa2820022@gmail.com>
2022-06-17 14:46:26 +0700

    optimizationn 2 :fix api sekai

--- controllers/sekaiController.js
+++ controllers/sekaiController.js
@@ -2,70 +2,90 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const download = require("image-downloader");
 const request = require("request");
-const winston = require('winston');
 
 const sharp = require("sharp");
 
 const sekaiUrl = "https://pjsekai.gamedbs.jp/chara/show/";
 const charUrl = "hhttps://pjsekai.gamedbs.jp/chara/show/1";
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
-});
+
 const downloadImg = async (req, res) => {
   let firstChar = 1;
 
   let endChar = 25;
-  const urlContainers = [];
+  const urlContainer = [];
+  const charUrlContainer = [];
+  let urlBack = [];
+
+
   for (firstChar; firstChar <= endChar; firstChar++) {
     let url = sekaiUrl + firstChar;
-    await axios(url).then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      $(" .uk-grid-match ").each(function () {
-        $(this)
-          .find("a")
-          .each(async function () {
-            let url = $(this).attr("href");
-
-            const response = await axios(url);
-            if (response) {
-              const html = response.data;
-              const $ = cheerio.load(html);
-              $(".uk-grid-match").each(function () {
-                $(this)
-                  .find("a")
-                  .each(function () {
-                    const url = $(this).attr("href");
-                    if (
-                      url.split(
-                        "https://pjsekai.gamedbs.jp/image/chara/"
-                      )[1] !== undefined
-                    ) {
-                      const output = Date.now() + "result.png";
-                       request({ url, encoding: null }, function (error, response, body) {
-                          sharp(body)
-                            .toFile(`/Pictures/live2D/shiny/${output}`)
-                            .then(() => {});
-                        }
-                      );
-                    }
-                  });
-              });
-            }
-          });
-      });
-    });
+    urlContainer.push(url);
   }
- 
+  const response = urlContainer.map((url) => {
+    return axios(url);
+  })
+  const result = await Promise.all(response);
+  result.map((res) => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+    $("div.uk-grid-match", html).each(function (i, e) {
+      $(this)
+        .find("a")
+        .each(async function () {
+          const url = $(this).attr("href");
+          charUrlContainer.push(url);
+        });
+    });
+  });
 
+  const response2 = charUrlContainer.map((url) => {
+    return axios(url);
+  })
+ 
+  const result2 = await Promise.all(response2)
+  
+  result2.map((response)=>{
+      
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $(".uk-grid-match").each(function () {
+      $(this)
+        .find("a")
+        .each(function () {
+          const url = $(this).attr("href");
+       
+          if(url.split(
+            "https://pjsekai.gamedbs.jp/image/chara/"
+          )[1] !== undefined && url.split("https://pjsekai.gamedbs.jp/image/chara/member/")[1]){
+            const image = url;
+            urlBack.push(image);
+            const output = Date.now() + "result.png";
+            request({ url, encoding: null }, function (error, response, body) {
+               sharp(body)
+                 .toFile(`/Pictures/live2D/shiny/${output}`)
+                 .then(() => {}).catch((err) => console.error(err))
+             }
+           );
+          }  
+     
+         
+           
+       
+        });
+    });
+  })
+
+        
+      
+    
+  
+ 
+ 
 
 
   res.status(200).json({
     message: "Success",
-    urlContainers: urlContainers,
+    urlBack  : urlBack
   });
 };
 
